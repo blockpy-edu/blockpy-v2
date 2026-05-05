3a0990ab6ff795d173d96f8937907adf238cebb8
Austin Cory Bart <acbart@vt.edu>
2026-05-05 17:01:29 -0400

    Include missing typedef

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL ?? '/',
   plugins: [react()],
   test: {
     globals: true,
